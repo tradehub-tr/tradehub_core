06615277d4ec6a370af8efba4c14b6ce0cbdc320
Bora <boraydeger32@gmail.com>
2026-04-08 15:57:52 +0300

    feat(notification): Platform Notification DocType ve notify yardımcısı eklendi

---
+++ tradehub_core/tradehub_core/doctype/platform_notification/platform_notification.py
@@ -0,0 +1,9 @@
+import frappe
+from frappe.model.document import Document
+
+
+class PlatformNotification(Document):
+
+    def before_save(self):
+        if self.is_read and not self.read_at:
+            self.read_at = frappe.utils.now()
