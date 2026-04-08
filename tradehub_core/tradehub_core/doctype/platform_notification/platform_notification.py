9d872b9a74bd1686dfdaa0ca36baaa5a927ccf19
Bora <turksab.yonetim@gmail.com>
2026-04-08 16:19:57 +0300

    Revert "feat(notification): Platform Notification DocType ve notify yardımcısı eklendi"
    This reverts commit c0bd7a04051ba82c77b1498ab8df8bcf77ca3070.

--- tradehub_core/tradehub_core/doctype/platform_notification/platform_notification.py
+++
@@ -1,9 +0,0 @@
-import frappe
-from frappe.model.document import Document
-
-
-class PlatformNotification(Document):
-
-    def before_save(self):
-        if self.is_read and not self.read_at:
-            self.read_at = frappe.utils.now()
